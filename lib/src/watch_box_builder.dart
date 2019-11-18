0f421f73ac43981c1423c742c174e462f33947dc
Kai Page <kaibug@gmail.com>
2019-11-18 15:48:55 -0600

    Add generic type parameter to WatchBoxBuilder

--- lib/src/watch_box_builder.dart
+++ lib/src/watch_box_builder.dart
@@ -1,11 +1,11 @@
 part of hive_flutter;
 
 /// Signature for a function that builds a widget given a [Box].
-typedef BoxWidgetBuilder = Widget Function(BuildContext context, Box box);
+typedef BoxWidgetBuilder<E> = Widget Function(BuildContext context, Box<E> box);
 
 /// A general-purpose widget which rebuilds itself when the box or a specific
 /// key change.
-class WatchBoxBuilder extends StatefulWidget {
+class WatchBoxBuilder<E> extends StatefulWidget {
   /// Creates a widget that rebuilds itself when a value in the [box] changes.
   ///
   /// If you specify [watchKeys], the widget only refreshes when a value
@@ -20,19 +20,19 @@ class WatchBoxBuilder extends StatefulWidget {
         super(key: key);
 
   /// The box which should be watched.
-  final Box box;
+  final Box<E> box;
 
   /// Called every time the box changes. The builder must not return null.
-  final BoxWidgetBuilder builder;
+  final BoxWidgetBuilder<E> builder;
 
   /// Specifies which keys should be watched.
   final List<String> watchKeys;
 
   @override
-  _WatchBoxBuilderState createState() => _WatchBoxBuilderState();
+  _WatchBoxBuilderState<E> createState() => _WatchBoxBuilderState<E>();
 }
 
-class _WatchBoxBuilderState extends State<WatchBoxBuilder> {
+class _WatchBoxBuilderState<E> extends State<WatchBoxBuilder<E>> {
   @visibleForTesting
   StreamSubscription subscription;
 
@@ -44,7 +44,7 @@ class _WatchBoxBuilderState extends State<WatchBoxBuilder> {
   }
 
   @override
-  void didUpdateWidget(WatchBoxBuilder oldWidget) {
+  void didUpdateWidget(WatchBoxBuilder<E> oldWidget) {
     super.didUpdateWidget(oldWidget);
 
     if (widget.box != oldWidget.box) {
